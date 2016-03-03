fc0d07aa3f5686b783a41171bebd7815d75c3743
Alfin Akhret <alfin@qasico.com>
2016-03-04 01:36:21 +0700

    update the CLI

--- server.js
+++ server.js
@@ -1,15 +1,62 @@
-var program = require('commander'),
+var prompt = require('prompt'),
+    program = require('commander'),
+    colors = require('colors'),
     Game = require('./game');
 
+
 program
-    .version('0.0.1')
-    .arguments('<start>')
-    .option('-p, --players <n>', 'Number of players to play in a game', parseInt)
-    .option('-l, --lucky-dice <n>', 'The lucky dice', parseInt)
-    .option('-b, --badluck-dice <n>', 'The bad luck dice', parseInt)
-    .action(function() {
-        // do something
-        
+    .arguments('<players>')
+    .arguments('<luckyDice>')
+    .arguments('<badluckDice>')
+    .action(function(players, luckyDice, badluckDice){
+        // create new game
+        var g = new Game(players, luckyDice, badluckDice);
+
+        prompt.message = colors.rainbow(">>");
+        prompt.delimiter = " ";
+                   
+        (function ask(round){
+
+            var property = {
+                name: 'start',
+                message: colors.green('start next round '+ round + ' ?'),
+                validator: /y[es]*|n[o]?/,
+                warning: 'Must respond yes or no',
+                default: 'yes',
+            };
+
+            prompt.get(property, function(err, result){
+                if (err) { return onErr(err); }
+                var answer = result.start;
+                if(answer === 'no' || answer === 'n') {
+                    console.log('we are done.');
+                } else {
+                    // play the game
+                    console.log('ROUND ' + round + '\n========================');
+                    g.start();
+                    console.log('After dice rolled\n------------------------');
+                    g.players.forEach(formatOutput);
+
+                    console.log('\n');
+                    console.log('After dice moved/removed\n------------------------');
+                    
+                    g.result();
+                    g.players.forEach(formatOutput);
+
+                    ask(g.round);
+                }
+            });
+        })(g.round);
+
+        function onErr(err) {
+            console.log(err);
+            return 1;
+        }
+
+        function formatOutput(num, index) {
+            console.log('Player'+ (index+1) + ' : ' + num.dices.toString());
+        }
+
     })
     .parse(process.argv);
 
